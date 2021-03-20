e7fd915e72be0d8efe8bdb9df83e7856b179091d
wangdana <wangdana1998@gmail.com>
2021-03-20 14:16:57 +0800

    写好三个icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,37 +2,57 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
 
+require('icons/money.svg');
+require('icons/label.svg');
+require('icons/statistics.svg');
 
 const NavWrapper = styled.div`
-box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   line-height: 24px;
 
   > ul {
     display: flex;
+
     > li {
       width: 33.3333%;
       text-align: center;
-      padding: 16px;
+      display: flex;
+      flex-direction: column;
+      padding: 4px 0;
+      align-items: center;
+      .icons {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
 
-const Nav=()=>{
-    return(
+const Nav = () => {
+    return (
         <NavWrapper>
             <ul>
                 <li>
-                    <Link to="/tags">标签页</Link>
+                    <svg className='icon'>
+                        <use xlinkHref='#label'/>
+                    </svg>
+                    <Link to="/tags">标签</Link>
                 </li>
                 <li>
-                    <Link to="/money">记账页</Link>
+                    <svg className='icon'>
+                        <use xlinkHref='#money'/>
+                    </svg>
+                    <Link to="/money">记账</Link>
                 </li>
                 <li>
-                    <Link to="/statistics">统计页</Link>
+                    <svg className='icon'>
+                        <use xlinkHref='#statistics'/>
+                    </svg>
+                    <Link to="/statistics">统计</Link>
                 </li>
             </ul>
         </NavWrapper>
-    )
-}
+    );
+};
 
-export default Nav
+export default Nav;
