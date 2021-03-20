e2a8a7ac4a612347b1853ff1e8e9a332be40bfae
wangdana <wangdana1998@gmail.com>
2021-03-20 13:26:30 +0800

    添加svg文件

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import {
     HashRouter as Router,
     Switch,
     Route,
-    Link,
     Redirect
 } from 'react-router-dom';
 import styled from 'styled-components';
+import Nav from './components/Nav';
+
 
 
 const Wrapper = styled.div`
@@ -21,20 +22,7 @@ const Main = styled.div`
   flex-grow: 1;
   overflow: auto;
 `;
-const Nav = styled.div`
-  border: 1px solid blue;
 
-  > ul {
-    display: flex;
-    > li {
-      width: 33.3333%;
-      text-align: center;
-      padding: 16px;
-    }
-  }
-
-
-`;
 
 function App() {
     return (
@@ -57,21 +45,7 @@ function App() {
                         </Route>
                     </Switch>
                 </Main>
-                <Nav>
-                    <ul>
-                        <li>
-                            <Link to="/tags">标签页</Link>
-                        </li>
-                        <li>
-                            <Link to="/money">记账页</Link>
-                        </li>
-                        <li>
-                            <Link to="/statistics">统计页</Link>
-                        </li>
-                    </ul>
-                </Nav>
-
-
+                <Nav/>
             </Wrapper>
         </Router>
     );
