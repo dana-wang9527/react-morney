6db107d5889216a6d59b7b88b89d5516eb7b3550
wangdana <wangdana1998@gmail.com>
2021-03-22 22:01:59 +0800

    tag list 样式做完

--- src/Views/Tags.tsx
+++ src/Views/Tags.tsx
@@ -1,14 +1,63 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import {useTags} from '../useTags';
+import styled from 'styled-components';
+import Icon from 'components/Icon';
+
+
+const TagList = styled.ol`
+  font-size: 16px;
+  background: white;
+
+  > li {
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #f60;
+  border-radius: 4px;
+  color: white;
+`;
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Space=styled.div`
+height: 48px;
+`;
 
 function Tags() {
-   const {tags,setTags} =useTags()
+    const {tags, setTags} = useTags();
     return (
         <Layout>
-            <h2>标签页面</h2>
+            <TagList>
+                {
+                    tags.map(tag =>
+                        <li key={tag}>
+                            <span>{tag}</span>
+                            <Icon name="Right"/>
+                        </li>
+                    )}
+            </TagList>
+            <Box>
+                <Space/>
+                <Button>新增标签</Button>
+            </Box>
+
         </Layout>
     );
 }
 
-export default Tags
+export default Tags;
