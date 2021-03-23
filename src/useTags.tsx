226d23d574adba0126a3ca760a208f5f2d9631be
wangdana <wangdana1998@gmail.com>
2021-03-23 17:28:39 +0800

    优化跟新和删除tag的代码

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -11,32 +11,14 @@ const deFaultTags = [
 const useTags = () => {//封装一个自定义Hook
     const [tags, setTags] = useState<{ id: number; name: string }[]>(deFaultTags);
     const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
-    const findTagIndex = (id: number) => {
-        let result = -1;
-        for (let i = 0; i < tags.length; i++) {
-            if (tags[i].id === id) {
-                result = i;
-                break;
-            }
-        }
-        return result;
-    };
     const upDateTag = (id: number, obj: { name: string }) => {
-        const index = findTagIndex(id);
-        //拷贝tags得到tagsClone
-        const tagsClone = JSON.parse(JSON.stringify(tags));
-        //把tagClone的第index删掉，换成{id：id, name: obj.name}
-        tagsClone.splice(index, 1, {id, name: obj.name});
-        setTags(tagsClone);
+        setTags(tags.map(tag => tag.id === id ? {id, name: obj.name} : tag));
     };
 
     const deleteTag = (id: number) => {
-        const index = findTagIndex(id);
-        const tagsClone = JSON.parse(JSON.stringify(tags));
-        tagsClone.splice(index, 1);
-        setTags(tagsClone);
+        setTags(tags.filter(tag => tag.id !== id));
     };
-    return {tags, setTags, findTag, upDateTag, findTagIndex, deleteTag};
+    return {tags, setTags, findTag, upDateTag, deleteTag};
 };
 
 export {useTags};
