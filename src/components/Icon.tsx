3542bbc48e1572dafe3948ba5021b5f22284a72c
wangdana <wangdana1998@gmail.com>
2021-03-27 22:13:57 +0800

    给删除图片和编辑图片添加样式

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import cs from 'classnames';
+import styled from 'styled-components';
 
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try {
@@ -7,16 +8,38 @@ try {
 } catch (error) {
     console.log(error);
 }
+
 type Props = {
-    name?: string
-}& React.SVGAttributes<SVGElement>
+    name: string
+} & React.SVGAttributes<SVGElement>
+
+const Wrapper = styled.div`
+  .icon {
+    &.delete {
+      width: 20px;
+      height: 20px;
+      color: #f00;
+      margin: 0 8px;
+    }
+
+    &.edit {
+      width: 20px;
+      height: 20px;
+      color: #00f;
+      margin: 0 8px;
+    }
+  }
+
+`;
 
 const Icon = (props: Props) => {
-    const {name,children,className,...rest}=props
+    const {name, children, className,...rest} = props;
     return (
-        <svg className={cs('icon',className)} {...rest}>
-            {props.name} &&<use xlinkHref={'#' + props.name}/>
-        </svg>
+        <Wrapper>
+            <svg className={cs('icon', className)} {...rest}>
+                {props.name} &&<use xlinkHref={'#' + props.name}/>
+            </svg>
+        </Wrapper>
     );
 };
 
